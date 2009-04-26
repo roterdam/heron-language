b0a91f2d4735263500be05d733bbc4a1b2883ebc
cdiggins <cdiggins@8e4626b8-fd44-0410-8dd2-c98a54c7fc1c>
2009-04-26 15:07:57 +0000

    Added HeronStandardLibrary. Reconfigured project and stuff.

---
+++ trunk/HeronStandardLibrary/ViewportForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Text;
+using System.Windows;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace HeronStandardLibrary
+{
+    public delegate void ViewportCmd(Graphics g);
+
+    public partial class ViewportForm : Form
+    {
+        public Bitmap bmp;
+        public Graphics g;
+
+        public ViewportForm(int w, int h)
+        {
+            InitializeComponent();
+            bmp = new Bitmap(w, h);
+            g = Graphics.FromImage(bmp);
+            Width = w;
+            Height = h;
+        }
+
+        public void ApplyToBitmap(ViewportCmd cmd)
+        {
+            if (InvokeRequired)
+            {
+                Delegate d = Delegate.CreateDelegate(GetType(), cmd, GetType().GetMethod("ApplyToBitmap"));
+                Invoke(cmd);
+            }
+            else
+            {
+                cmd(g);
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImageUnscaled(bmp, 0, 0);
+        }
+
+        private void ViewportForm_Paint(object sender, PaintEventArgs e)
+        {
+            // ??
+        }
+    }
+}
