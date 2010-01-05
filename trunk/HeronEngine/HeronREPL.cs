37b5d3c98f43ea4b26ca4983aee5ca3bbe6d3030
cdiggins <cdiggins@8e4626b8-fd44-0410-8dd2-c98a54c7fc1c>
2010-01-05 03:31:26 +0000

    Starting work on a possible REPL for Heron.

---
+++ trunk/HeronEngine/HeronREPL.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace HeronEngine
+{
+    public class REPL 
+    {
+        VM vm = new VM();
+        bool bExit = false;
+        string sPrompt = ">>";
+
+        public REPL()
+        {
+            vm.AddVar("vm", vm);
+            RegisterFunction("Exit");
+            Print("Welcome to the Heron interactive interpreter");
+            Print("To exit, type in 'Exit()' at the prompt");
+            Print("");
+        }
+
+        public void RegisterFunction(string s)
+        {
+            MethodInfo mi = GetType().GetMethod(s);
+            DotNetMethod m = new DotNetMethod(mi, DotNetObject.Marshal(this));
+            vm.AddVar(s, m);
+        }
+
+        public void Run()
+        {
+            while (!bExit)
+                Print(Eval(Read()));
+        }
+
+        public void Exit()
+        {
+            bExit = true;
+        }
+
+        public string Read()
+        {
+            Console.Write(sPrompt);
+            return Console.ReadLine();
+        }
+
+        public string Eval(string s)
+        {
+            s = s.TrimEnd();
+            throw new NotImplementedException();
+        }
+
+        public void Print(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
