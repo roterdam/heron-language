2cf5727bd02f20ad2cf1f84e1f4b1b3c99a5590b
cdiggins <cdiggins@8e4626b8-fd44-0410-8dd2-c98a54c7fc1c>
2010-02-28 21:18:22 +0000

    Cleaning up repository.

---
+++ HeronEngine/HeronBinaryOperation.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace HeronEngine
+{
+    delegate HeronValue BinaryOperator(HeronValue a, HeronValue b);
+
+    public enum OpCode
+    {
+        opAdd,
+        opSub,
+        opMul,
+        opDiv,
+        opMod,
+        opShr,
+        opShl,
+        opGtEq,
+        opGt,
+        opLtEq,
+        opLt,
+        opRange,
+        opEq,
+        opNEq,
+        opIs,
+        opAs,
+        opAnd,
+        opOr,
+        opXOr,
+    };
+
+    class BinaryOperation : Expression
+    {
+        public static OpCode StringToOpCode(string s)
+        {
+            switch (s)
+            {
+                case "+": return OpCode.opAdd;
+                case "-": return OpCode.opSub;
+                case "*": return OpCode.opMul;
+                case "/": return OpCode.opDiv;
+                case "%": return OpCode.opMod;
+                case ">>": return OpCode.opShr;
+                case "<<": return OpCode.opShl;
+                case ">=": return OpCode.opGtEq;
+                case ">": return OpCode.opGt;
+                case "<=": return OpCode.opLtEq;
+                case "<": return OpCode.opLt;
+                case "..": return OpCode.opRange;
+                case "==": return OpCode.opEq;
+                case "!=": return OpCode.opNEq;
+                case "is": return OpCode.opIs;
+                case "as": return OpCode.opAs;
+                case "&&": return OpCode.opAnd;
+                case "||": return OpCode.opOr;
+                case "^^": return OpCode.opXOr;
+            }
+            throw new Exception("Unrecognized operation " + s);
+        }
+
+        [HeronVisible] public Expression operand1;
+        [HeronVisible] public Expression operand2;
+        [HeronVisible] public string operation;
+        public OpCode opcode;
+
+        public BinaryOperation(string sOp, Expression x, Expression y)
+        {
+            operation = sOp;
+            opcode = StringToOpCode(sOp);
+            operand1 = x;
+            operand2 = y;
+        }
+
+
+        public override HeronValue Eval(VM vm)
+        {
+            HeronValue a = operand1.Eval(vm);
+            HeronValue b = operand2.Eval(vm);
+
+            switch (opcode)
+            {
+                case OpCode.opEq:
+                    return new BoolValue(a.Equals(b));
+                case OpCode.opNEq:
+                    return new BoolValue(!a.Equals(b));
+                case OpCode.opIs:
+                    {
+                        HeronType t = b as HeronType;
+                        if (t == null)
+                            throw new Exception("The second argument of the 'is' operator must be a type");
+                        return new BoolValue(a.Is(t));
+                    }
+                case OpCode.opAs:
+                    {
+                        HeronType t = b as HeronType;
+                        if (t == null)
+                            throw new Exception("The 'as' operator expects a type as a right hand argument");
+                        HeronValue r = a.As(t);
+                        if (r != null)
+                            return r;
+                        if (t is InterfaceDefn && a is ClassInstance)
+                        {
+                            DuckValue dv = new DuckValue(a as ClassInstance, t as InterfaceDefn);
+                            return dv;
+                        }
+                        throw new Exception("Failed to convert " + a.GetHeronType().name + " to a " + t.name);
+                    };
+            }
+
+            return a.InvokeBinaryOperator(vm, opcode, b);
+        }
+
+        public override string ToString()
+        {
+            return "(" + operand1.ToString() + " " + operation + " " + operand2.ToString() + ")";
+        }
+
+        public override HeronType GetHeronType()
+        {
+            return PrimitiveTypes.BinaryOperation;
+        }
+
+        public override Expression Optimize(OptimizationParams op)
+        {
+            return new BinaryOperation(operation, operand1.Optimize(op), operand2.Optimize(op));
+        }
+    }
+}
---
+++ HeronEngine/HeronMapExpr.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq.Parallel;
+using System.Diagnostics;
+using System.Collections.Concurrent;
+
+namespace HeronEngine
+{
+    /// <summary>
+    /// Represents ta map expression.
+    /// This transforms ta list of N items into ta list of N items by 
+    /// applying ta unary function to each item in the list.
+    /// </summary>
+    public class MapExpr : Expression
+    {
+        [HeronVisible] public string name;
+        [HeronVisible] public Expression list;
+        [HeronVisible] public Expression yield;
+
+        public MapExpr(string name, Expression list, Expression yield)
+        {
+            this.name = name;
+            this.list = list;
+            this.yield = yield;
+        }
+
+        public override Expression Optimize(OptimizationParams op)
+        {
+            return this;
+        }
+         
+        public override HeronValue Eval(VM vm)
+        {   
+            IInternalIndexable ii = vm.EvalInternalList(list);
+            HeronValue[] output = new HeronValue[ii.InternalCount()];
+            ParallelOptions po = new ParallelOptions();
+            po.MaxDegreeOfParallelism = Config.maxThreads;
+            var p = Partitioner.Create(0, ii.InternalCount());
+            Parallel.ForEach(
+                p,
+                po,
+                () =>
+                {
+                    LoopParams lp = new LoopParams();
+                    lp.op = new OptimizationParams();
+                    lp.acc = lp.op.AddNewAccessor(name);
+                    lp.vm = vm.Fork();
+                    lp.expr = yield.Optimize(lp.op);
+                    return lp;
+                },
+                (Tuple<int, int> range, ParallelLoopState state, LoopParams lp) =>
+                {
+                    for (int i = range.Item1; i < range.Item2; ++i)
+                    {
+                        lp.acc.Set(ii.InternalAt(i));
+                        output[i] = lp.vm.Eval(lp.expr);
+                    }
+                    return lp;
+                },
+                (LoopParams lp) => { }
+                );
+
+            return new ArrayValue(output);
+        }
+
+        public override string ToString()
+        {           
+            return "map (" + name + " in " + list.ToString() + ") to " + yield.ToString();
+        }
+
+        public override HeronType GetHeronType()
+        {
+            return PrimitiveTypes.MapExpr;
+        }
+    }
+}
